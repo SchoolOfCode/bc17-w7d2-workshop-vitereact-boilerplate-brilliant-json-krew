b8b7c2ce2bbe56bc79b206006b58a3cec41fc3df
Jonathan Hill <144107180+MrHighViolet@users.noreply.github.com>
2024-08-13 12:31:29 +0100

    footer started

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function FooterComponent() {
   return (
-    <div>FooterComponent</div>
+    <div>
+      <footer>
+              <section className ="footer-container">
+                <article>
+                  <ul>
+                    <li>Find us on:</li>
+                    <li><a href="https://www.facebook.com" class="social-link">Facebook</a></li>
+                    <li><a href="https://www.instagram.com" class="social-link">Instagram</a></li>
+                    <li><a href="https://www.tiktok.com" class="social-link">TikTok</a></li> 
+                  </ul>           
+                </article>
+                  
+                <article>  
+                <p>Contact us:</p>
+                <address><a href="mailto:info@firepalace.co.uk" class="email-link"
+                >info@firepalace.co.uk</a></address>
+                </article>
+              </section>
+      </footer>
+    </div>
   )
 }
