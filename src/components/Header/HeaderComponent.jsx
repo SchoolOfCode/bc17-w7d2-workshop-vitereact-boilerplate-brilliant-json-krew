341e6d66a5c5fc2fd96799b214dc95b4dcd3cc51
Kiran Gadhavi <kirangadhavi@hotmail.com>
2024-08-13 14:02:44 +0100

    completed mobile version

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './style.css'
+import './styleHeader.css'
 
 export default function HeaderComponent() {
   return (
--- src/components/Main/MainComponent.jsx
+++ src/components/Main/MainComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.css';
+import './styleMain.css';
 
 export default function MainComponent() {
   return (
